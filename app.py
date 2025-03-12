87e2e6623225ccd1387a0f33167f60b6079967f2
Leandro von Werra <lvwerra@users.noreply.huggingface.co>
2025-03-12 15:37:30 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 from e2b_desktop import Sandbox
 
 
-E2B_API_KEY = os.getenvs("E2B_API_KEY")
+E2B_API_KEY = os.getenv("E2B_API_KEY")
 print(f"e2b (og): '{E2B_API_KEY}'")
 DEFAULT_MAX_TOKENS = 512
 SANDBOXES = {}
