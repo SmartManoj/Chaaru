b5574b76aae92742999a72cf01345757cce39397
Aymeric <aymeric.roucher@gmail.com>
2025-05-06 15:41:34 +0200

    Improve notes

--- app.py
+++ app.py
@@ -611,9 +611,9 @@ _Please note that we store the task logs by default so **do not write any person
 
             gr.Markdown(
                 """
-- **Data**: You can opt-out of storing your trace if you uncheck the box above.
-- **Time**: The agent's first step can take a few seconds. Be patient!
-- **Captcha**: Sometimes the VMs get flagged for weird behaviour and are blocked with a captcha. The models are not quite there, yet - best to interrupt the agent and solve the captcha manually.
+- **Data**: To opt-out of storing your trace, uncheck the box above.
+- **Be patient**: The agent's first step can take a few seconds.
+- **Captcha**: Sometimes the VMs get flagged for weird behaviour and are blocked with a captcha. Best is then to interrupt the agent and solve the captcha manually.
 - **Restart**: If your agent seems stuck, the simplest way to restart is to refresh the page.
                 """.strip()
             )
